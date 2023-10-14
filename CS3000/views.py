4b8ac84724c1ba8d44bf7ad2c8231c083d14975a
SyraDominguez <syradominguez.dev@gmail.com>
2023-10-14 20:08:08 +0200

    feat: extiendo estilos a las paginas

--- CS3000/views.py
+++ CS3000/views.py
@@ -3,5 +3,25 @@ from . import app
 
 
 @app.route('/')
-def home():
+def entrar():
+    return render_template('base.html')
+
+
+@app.route('/home')
+def iniciar():
+    return render_template('home.html')
+
+
+@app.route('/operations')
+def operar():
+    return render_template('operations.html')
+
+
+@app.route('/status')
+def checking():
+    return render_template('status.html')
+
+
+@app.route('/base')
+def logout():
     return render_template('base.html')
