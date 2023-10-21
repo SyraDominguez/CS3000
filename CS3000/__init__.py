ebf984f410ef85ff69169d9ea7f449a61f98453f
SyraDominguez <syradominguez.dev@gmail.com>
2023-10-21 11:19:22 +0200

    fix: corrijo error CORS

--- CS3000/__init__.py
+++ CS3000/__init__.py
@@ -1,4 +1,13 @@
 from flask import Flask
 
+from flask_cors import CORS
+
+
 app = Flask(__name__)
 app.config.from_object('config')
+
+cors = CORS(app, resources={
+    r'/api/*': {
+        'origins': '*'
+    }
+})
