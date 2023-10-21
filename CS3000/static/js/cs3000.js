2d8806e93f86da9e01947d61423c7ee75d5feabd
SyraDominguez <syradominguez.dev@gmail.com>
2023-10-21 11:18:50 +0200

    feat: llamo a la API desde JS

--- CS3000/static/js/cs3000.js
+++ CS3000/static/js/cs3000.js
@@ -1,9 +1,15 @@
+const peticion = new XMLHttpRequest();
+
 function cargarMovimientos() {
-    console.log('Has llamado a la funcion de Cargar Movimientos')
+    console.log('Has llamado a la funcion de Cargar Movimientos');
+    peticion.open('GET', 'http://localhost:5000/api/v1/movimientos', false);
+    peticion.send();
+    console.log(peticion.responseText);
 }
 
 window.onload = function() {
+    console.log('Ya se han cargado los elementos de la pagina');
     const boton = document.getElementById('boton-recarga');
     boton.addEventListener('click', cargarMovimientos);
-    cargarMovimientos();
+    console.log('FIN de la funcion window.onload');
 }
