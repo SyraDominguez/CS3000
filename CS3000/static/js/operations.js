a3ac60a83e21badd16288fa03ce649137fbca1d3
SyraDominguez <syradominguez.dev@gmail.com>
2023-10-24 11:22:30 +0200

    feat: reahago obtencion exchange e inyeccion de datos en pantalla

--- CS3000/static/js/operations.js
+++ CS3000/static/js/operations.js
@@ -1,13 +1,15 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-    //Deshabilitar las monedas
+// Deshabilitar las monedas
 
+    // Crear una nueva solicitud XMLHttpRequest.
     const peticion = new XMLHttpRequest();
-    
+
+    // Obtener elementos del formulario select 'Coin From' y 'Coin To'.
     const coinFromSelect = document.getElementById('coin-from');
     const coinToSelect = document.getElementById('coin-to');
 
-    // 1. Deshabilitar 'EUR' en 'Coin To' y 'BTC' en 'Coin From' al cargar la página
+    // 1. Deshabilitar 'EUR' en 'Coin To' y 'BTC' en 'Coin From' al cargar la página.
     const coinToOptions = coinToSelect.querySelectorAll('option');
     const coinFromOptions = coinFromSelect.querySelectorAll('option');
 
@@ -27,8 +29,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 
-
-    // 2. Cuando se elija una moneda en 'Coin From', deshabilitar la misma moneda en 'Coin To'
+    // 2. Cuando se elija una moneda en 'Coin From', deshabilitar la misma moneda en 'Coin To'.
     coinFromSelect.addEventListener('change', function() {
         const coinFrom = this.value;
         const coinToOptions = coinToSelect.querySelectorAll('option');
@@ -42,7 +43,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 
-    // 3. Cuando se elija una moneda en 'Coin To', deshabilitar la misma moneda en 'Coin From'
+    // 3. Cuando se elija una moneda en 'Coin To', deshabilitar la misma moneda en 'Coin From'.
     coinToSelect.addEventListener('change', function() {
         const coinTo = this.value;
         const coinFromOptions = coinFromSelect.querySelectorAll('option');
@@ -56,45 +57,32 @@ window.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 
-    // Obtener el precio de una moneda from en moneda to
+    // Función para obtener el precio de una moneda from en moneda to.
     async function getExchangeRate(coinFrom, coinTo) {
+
         const response = await fetch(`/operations/${coinFrom}/${coinTo}`);
         const data = await response.json();
+
         return data.rate;
     }
 
-    // Inyectar los datos obtenidos en la pagina
-    function injectData(data) {
-        const puField = document.getElementById('p-u');
-        puField.value = data.rate;
+    // Función para inyectar los datos obtenidos en la página.
+    function injectExchangeRate(amountInvest, coinFrom, coinTo, rate, amountAcquired) {
+        const puInput = document.getElementById('pu');
+        const amountAcquiredInput = document.getElementById('amount_acquired');
 
-        const amountToField = document.getElementById('quantity-to');
-        amountToField.value = data.amount_to;
+        puInput.value = rate;
+        amountAcquiredInput.value = amountInvest * rate;
     }
 
-    // Vincular el evento del click del boton 'calculate'
-    
-    const calculateButton = document.getElementById('calculate-button');
-    calculateButton.addEventListener('click', async () => {
+    // Vincular todos estos calculos al evento click del botón 'calculate'.
+    document.getElementById('calculate-button').addEventListener('click', async () => {
         const coinFrom = document.getElementById('coin-from').value;
         const coinTo = document.getElementById('coin-to').value;
-        const quantityFrom = parseFloat(document.getElementById('quantity-from').value);
+        const amountInvest = parseFloat(document.getElementById('amount_invest').value);
 
         const rate = await getExchangeRate(coinFrom, coinTo);
-        const amountTo = quantityFrom * rate;
-
-        const data = {
-            rate,
-            amount_to: amountTo
-        };
-
-        injectData(data);
+        injectExchangeRate(amountInvest, coinFrom, coinTo, rate);
     });
 
-    
-
-
-    
-
 });
-
